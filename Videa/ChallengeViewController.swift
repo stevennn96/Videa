ff9fd3016cb383294b29aae4d6499f62b5df5649
Steven - <stevenjusuf9@gmail.com>
2018-08-20 17:23:10 +0700

    Challenge Page

---
+++ Videa/ChallengeViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ChallengeViewController.swift
+//  Videa
+//
+//  Created by Steven - on 20/08/18.
+//  Copyright © 2018 Steven. All rights reserved.
+//
+
+import UIKit
+
+class ChallengeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    let challenges = ["Introduction", "Music", "Comedy", "Food", "How To"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (challenges.count)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ViewControllerTableViewCell
+        
+            cell.myImage.image = UIImage(named: (challenges[indexPath.row] + ".jpg"))
+            cell.myLabel.text = challenges[indexPath.row]
+        
+            return (cell)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Videa/ViewControllerTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ViewControllerTableViewCell.swift
+//  Videa
+//
+//  Created by Steven - on 20/08/18.
+//  Copyright © 2018 Steven. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var myImage: UIImageView!
+    @IBOutlet weak var myLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
