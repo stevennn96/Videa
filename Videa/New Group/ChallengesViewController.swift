ed0502a74da74e2c36e9b5304e974065826eeb8a
Sia Feri Santos <siaferi.santos@yahoo.com>
2018-08-23 15:29:01 +0700

    Baru view controller

---
+++ Videa/New Group/ChallengesViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ChallengesViewController.swift
+//  test
+//
+//  Created by Sia Feri Santos on 22/08/18.
+//  Copyright © 2018 Sia Feri Santos. All rights reserved.
+//
+
+import UIKit
+
+class ChallengesViewController: UIViewController {
+
+    @IBAction func introduction(_ sender: Any) {
+        performSegue(withIdentifier: "goToIntroduction", sender: self)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
