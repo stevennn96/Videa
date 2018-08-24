c0774c5383cdb445083ae3951e793726d98752d8
Kensen Tjoa <kensen.tjoa@gmail.com>
2018-08-24 16:45:59 +0700

    Updated Statistics Page

---
+++ Videa/Kensen/StatisticsDataTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  StatisticsDataTableViewCell.swift
+//  Videa
+//
+//  Created by Kensen Tjoa on 24/08/18.
+//  Copyright © 2018 Steven. All rights reserved.
+//
+
+import UIKit
+
+class StatisticsDataTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var totalSubscribersLabel: UILabel!
+    @IBOutlet weak var totalViewsLabel: UILabel!
+    
+    func setStatisticsData(statisticsData: StatisticsData) {
+        dateLabel.text = statisticsData.date
+        totalSubscribersLabel.text = "\(statisticsData.totalSubscribers)"
+        totalViewsLabel.text = "\(statisticsData.totalViews)"
+    }
+}
