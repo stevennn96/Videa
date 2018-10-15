0f061d3d7d78e8107db9d4d4c134ebdbd4a1f9ff
Sia Feri Santos <siaferi.santos@yahoo.com>
2018-10-15 17:23:18 +0700

    edit quick tips dan edit profile

---
+++ Videa/Controller/QuickTips4ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  QuickTips4ViewController.swift
+//  Videa
+//
+//  Created by Sia Feri Santos on 15/10/18.
+//  Copyright © 2018 Steven. All rights reserved.
+//
+
+import UIKit
+
+class QuickTips4ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
